6738466b3aec693db036f365273bc36c4f260751
Muhammad Attiq <muhammad.attiq.computer.engineer@gmail.com>
2025-10-20 05:54:17 -0700

    Create Mirror_Check.asm

---
+++ MIPS_Assembly_Codes/Mirror_Check.asm
@@ -0,0 +1,55 @@
+.data
+prompt: .asciiz " Enter a 6 bit number \n "
+yes_msg: .asciiz "mirror image"
+no_msg: .asciiz "not mirror image"
+.text
+main:
+li $v0, 4
+la $a0, prompt
+syscall
+
+li $v0, 5
+syscall
+move $s0, $v0 
+
+andi $t0, $s0, 1
+
+srl $t1, $s0, 1
+andi $t1, $t1, 1
+
+srl $t2, $s0, 2
+andi $t2, $t2, 1
+
+srl $t3, $s0, 3
+andi $t3, $t3, 1
+
+srl $t4, $s0, 4
+andi $t4, $t4, 1
+
+srl $t5, $s0, 5
+andi $t5, $t5, 1
+
+and $t6, $t0, $t5
+and $t7, $t1, $t4
+and $s1, $t2, $t3
+
+or $t8, $t6, $t7
+or $t8, $t8, $s1
+
+bnez $t8, yes
+j no
+
+yes:
+li $v0, 4
+la $a0, yes_msg
+syscall
+j end
+
+no:
+li $v0, 4
+la $a0, no_msg
+syscall
+
+end:
+li $v0, 10
+syscall
