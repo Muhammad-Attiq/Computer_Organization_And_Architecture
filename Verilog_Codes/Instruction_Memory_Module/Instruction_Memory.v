8b1a42ef9c370f944b9be114426ab65fde00895b
Muhammad Attiq <muhammad.attiq.computer.engineer@gmail.com>
2025-11-10 05:35:35 -0800

    Update Instruction_Memory.v

--- Verilog_Codes/Instruction_Memory_Module/Instruction_Memory.v
+++ Verilog_Codes/Instruction_Memory_Module/Instruction_Memory.v
@@ -7,12 +7,12 @@ module Instruction_Memory(
   initial begin
     memory[0] = 32'h8c0c0000;
     memory[1] = 32'h8c0d0001;
-    memory[2]  = 32'h8c0e0002;  
-    memory[3]  = 32'h8c0f0003;  
-    memory[4]  = 32'h01ad4820;  
-    memory[5]  = 32'h01cf5020;  
-    memory[6]  = 32'h01494022;  
-    memory[7]  = 32'hac080004;  
+    memory[2] = 32'h8c0e0002;  
+    memory[3] = 32'h8c0f0003;  
+    memory[4] = 32'h01ad4820;  
+    memory[5] = 32'h01cf5020;  
+    memory[6] = 32'h01494022;  
+    memory[7] = 32'hac080004;  
   end
 
   always@(*) begin
