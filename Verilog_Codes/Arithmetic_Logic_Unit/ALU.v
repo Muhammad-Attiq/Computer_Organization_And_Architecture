6e6b6ccdfbf587f513ea995897612a08c46ed619
Muhammad Attiq <muhammad.attiq.computer.engineer@gmail.com>
2025-12-02 04:15:08 -0800

    Create ALU.v

---
+++ Verilog_Codes/Arithmetic_Logic_Unit/ALU.v
@@ -0,0 +1,21 @@
+module ALU(
+    input  [31:0] A,
+    input  [31:0] B,
+    input  [3:0]  ALUControl,
+    output reg [31:0] Result,
+    output Zero
+);
+
+    assign Zero = (Result == 0);
+
+    always @(*) begin
+        case (ALUControl)
+            4'b0010: Result = A + B;           // ADD
+            4'b0110: Result = A - B;           // SUB
+            4'b0000: Result = A & B;           // AND
+            4'b0001: Result = A | B;           // OR
+            4'b0111: Result = (A < B) ? 1 : 0; // SLT
+            default: Result = 0;
+        endcase
+    end
+endmodule
