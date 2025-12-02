002c62f354004935dbd22fb00d095e4e4af60277
Muhammad Attiq <muhammad.attiq.computer.engineer@gmail.com>
2025-12-02 04:27:00 -0800

    Create tb_Integration.v

---
+++ Verilog_Codes/Control_&_ALU_Module/tb_Integration.v
@@ -0,0 +1,30 @@
+module tb_Integration();
+
+    reg [31:0] A, B;
+    reg [5:0] opcode, funct;
+
+    wire RegDst, Branch, MemRead, MemtoReg, MemWrite, ALUSrc, RegWrite;
+    wire [1:0] ALUOp;
+    wire [3:0] ALUControlOut;
+    wire [31:0] Result;
+    wire Zero;  
+
+    Control ctrl(opcode, RegDst, Branch, MemRead, MemtoReg,
+                 ALUOp, MemWrite, ALUSrc, RegWrite);
+
+    ALUControl alu_ctrl(ALUOp, funct, ALUControlOut);
+
+    ALU alu(A, B, ALUControlOut, Result, Zero);
+
+    initial begin
+        A = 9; B = 4;
+
+        opcode = 6'b000000; funct = 6'b100000; #10; 
+        funct = 6'b100010; #10;                     
+
+        opcode = 6'b100011; #10; 
+        opcode = 6'b000100; #10;
+
+        $stop;
+    end
+endmodule
